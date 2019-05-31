012f8081e07d299f7e1980a3523d38714fa0ada2
Daniil Sharko <integrex@yandex.ru>
2019-05-31 16:36:02 +0500

    Remove trash line

--- src/deploy/builder/builder/urls.py
+++ src/deploy/builder/builder/urls.py
@@ -1,8 +1,6 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 
-from .views import MyView
-
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^stacks/', include(('stacks.urls', 'stacks'), namespace='stacks')),
